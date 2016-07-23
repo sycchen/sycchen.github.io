378776e068a05c3960e1341ef607bf4b7523b0c2
Steven Chen <Steven Chen>
2016-07-23 00:20:38 -0400

    Added contact page. Still needs server functionality.

--- index.js
+++ index.js
@@ -28,9 +28,53 @@ myApp.config(['$routeProvider', function($routeProvider){
         })
         .when('/contact', {
             templateUrl: '/app/pages/contact.html',
+            controller: 'ContactController',
             activeLink: 'Contact'
         })
         .otherwise({redirectTo:'/'});
 }]).controller("HeaderController", ['$scope', '$route', function($scope, $route) {
     $scope.$route = $route;
+}]).controller("ContactController", ['$scope', function($scope) {
+    $scope.successAlert = false;
+    $scope.errorAlert = false;
+
+    $scope.contactMethod = 'email';
+    $scope.email = '';
+    $scope.message = '';
+
+    $scope.hideSuccessAlert = function() {
+        $scope.successAlert = false;
+    }
+
+    $scope.hideErrorAlert = function() {
+        $scope.errorAlert = false;
+    }
+
+    $scope.sendMessage = function() {
+        var data;
+
+        switch ($scope.contactMethod) {
+            case 'email':
+                data = {
+                    contact: {
+                        email: $scope.email
+                    },
+                    message: $scope.message
+                };
+                break;
+            case 'anonymous':
+                data = {
+                    message: $scope.message
+                };
+                break;
+        }
+
+        // TODO:: Send data to server
+        var jsonPackage = JSON.stringify(data);
+
+        // Wipe data to prevent resending by accident
+        $scope.email = '';
+        $scope.message = '';
+        $scope.successAlert = true;
+    }
 }]);
