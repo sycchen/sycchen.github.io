1eb53f07ceb8bda2cd9a6908bec31a031ef5e413
Steven Chen <Steven Chen>
2016-07-28 02:48:42 -0400

    Home page implementation started.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ myApp.config(['$routeProvider', function($routeProvider){
     $routeProvider
         .when('/', {
             templateUrl: '/app/pages/home.html',
+            controller: 'HomeController',
             activeLink: 'Home'
         })
         .when('/projects', {
@@ -77,4 +78,30 @@ myApp.config(['$routeProvider', function($routeProvider){
         $scope.message = '';
         $scope.successAlert = true;
     }
+}]).controller("HomeController", ['$scope', function($scope) {
+    $scope.projects = [];
+
+    var getPagePreview = function(link) {
+        return "Insert Preview Here";
+    }
+
+    var init = function() {
+        $.ajax({
+            url: "/app/pages/projects/",
+            success: function(data) {
+                $(data).find("a:contains(.html)").each(function() {
+                    var link = $(this).attr("href");
+                    $scope.projects.push(
+                        {   href: "#" + link.replace(/\/app\/pages(.*)\.html/i,
+                                            function(match, p1, offset, string) {return p1}),
+                            preview: getPagePreview(link)}
+                    );
+                });
+
+                $scope.$apply();
+            }
+        });
+    };
+
+    init();
 }]);
